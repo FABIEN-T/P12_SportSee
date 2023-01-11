859ee4ae54fde9d3645739dd5d4f56902a136bbe
fabT <fabien.varlet@live.fr>
2023-01-11 16:07:08 +0100

    initiliasation projet

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  console.log('ceciEstUneErreur')
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>coucou !!!!</h1>
+        <p>
+          Cela permet de préciser à VSCode de formater le fichier uniquement
+          quand une configuration est présente dans le projet pour Prettier.
+          Donc à nous d'en créer une ! À la racine de votre projet, créez un
+          fichier  .prettierrc  dans lequel vous allez pouvoir préciser quelques
+          règles.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
