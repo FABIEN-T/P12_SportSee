11b6b18206a7fa59c8793f463f666773e3d71cf9
fabT <fabien.varlet@live.fr>
2023-01-16 11:51:58 +0100

    installation Recharts

--- src/pages/Setting/index.jsx
+++ src/pages/Setting/index.jsx
@@ -1,5 +1,16 @@
 function Setting() {
-  return <h1>Réglages</h1>
+  const data = [
+    { x: 1, y: 23, z: 122 },
+    { x: 2, y: 3, z: 73 },
+    { x: 3, y: 15, z: 32 },
+    { x: 4, y: 35, z: 23 },
+    { x: 5, y: 45, z: 20 },
+    { x: 6, y: 25, z: 29 },
+    { x: 7, y: 17, z: 61 },
+    { x: 8, y: 32, z: 45 },
+    { x: 9, y: 43, z: 93 },
+  ]
+  return <h1>Rechart</h1>
 }
 
 export default Setting
